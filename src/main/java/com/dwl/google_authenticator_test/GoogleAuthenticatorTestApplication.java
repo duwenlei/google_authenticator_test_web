186f1f1df2f4636662139369c04a3cb6e91e8695
du_wenlei <952589031@qq.com>
2019-01-23 18:14:38 +0800

    first commit

---
+++ src/main/java/com/dwl/google_authenticator_test/GoogleAuthenticatorTestApplication.java
@@ -0,0 +1,14 @@
+package com.dwl.google_authenticator_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAuthenticatorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleAuthenticatorTestApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/dwl/google_authenticator_test/GoogleAuthenticatorTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dwl.google_authenticator_test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoogleAuthenticatorTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
